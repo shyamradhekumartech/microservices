f1e4aa0869d2170805cf27fd1ed8dafcd9d7cbdc
sandeeprawat983 <rawat683sandeep@gmail.com>
2026-02-28 08:36:40 +0530

    configure H2 DB and YAML properties

--- section2/accounts/src/main/java/com/moneybanks/accounts/controller/AccountController.java
+++ section2/accounts/src/main/java/com/moneybanks/accounts/controller/AccountController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AccountController {
 
-    @GetMapping("/")
+    @GetMapping("/sayHello")
     public String sayHello() {
         return "Hello World";
     }
