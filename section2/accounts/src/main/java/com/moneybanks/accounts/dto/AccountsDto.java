fb5d0a02c8dd12ee2206ddd24f1b80a2fc03b6f3
sandeeprawat983 <rawat683sandeep@gmail.com>
2026-03-01 15:58:04 +0530

    added Input validation at DTO and controller

--- section2/accounts/src/main/java/com/moneybanks/accounts/dto/AccountsDto.java
+++ section2/accounts/src/main/java/com/moneybanks/accounts/dto/AccountsDto.java
@@ -1,11 +1,18 @@
 package com.moneybanks.accounts.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+
 public class AccountsDto {
 
+    @NotEmpty(message = "AccountNumber cannot be a null or empty")
+    @Pattern(regexp = "(^$|[0-9]{10})", message = "AccountNumber must be 10 digits")
     private Long accountNumber;
 
+    @NotEmpty(message = "AccountType cannot be a null or empty")
     private String accountType;
 
+    @NotEmpty(message = "BranchAddress cannot be a null or empty")
     private String branchAddress;
 
     public Long getAccountNumber() {
--- section2/accounts/src/main/java/com/moneybanks/accounts/exception/GlobalExceptionHandler.java
+++ section2/accounts/src/main/java/com/moneybanks/accounts/exception/GlobalExceptionHandler.java
@@ -1,16 +1,25 @@
 package com.moneybanks.accounts.exception;
 
 import com.moneybanks.accounts.dto.ErrorResponseDto;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponseDto> handleGlobalException(Exception exception, WebRequest webRequest) {
@@ -46,6 +55,19 @@ public class GlobalExceptionHandler {
         );
 
         return new ResponseEntity<>(errorResponseDto, HttpStatus.NOT_FOUND);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        Map<String, String> validationErrors = new HashMap<>();
+        List<ObjectError> validationErrorList = ex.getBindingResult().getAllErrors();
+
+        validationErrorList.forEach(objectError -> {
+            String fieldName = ((FieldError)objectError).getField();
+            String validationMsg = objectError.getDefaultMessage();
+            validationErrors.put(fieldName, validationMsg);
+        });
+        return new ResponseEntity<>(validationErrors, HttpStatus.BAD_REQUEST);
     }
 
 }
