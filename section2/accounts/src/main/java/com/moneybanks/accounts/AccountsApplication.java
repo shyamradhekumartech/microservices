6986ac89bf9eba6ad0cf51c30a20297a3c02061f
sandeeprawat983 <rawat683sandeep@gmail.com>
2026-03-02 14:15:22 +0530

    enhance the documentation of REST APIs

--- section2/accounts/src/main/java/com/moneybanks/accounts/AccountsApplication.java
+++ section2/accounts/src/main/java/com/moneybanks/accounts/AccountsApplication.java
@@ -1,11 +1,36 @@
 package com.moneybanks.accounts;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Accounts microservice REST API documentation",
+				description = "MoneyBank Accounts microservice REST API documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Sandeep Rawat",
+						email = "microservice@abcd.com",
+						url = "http://localhost:8080/swagger-ui/index.html#/"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "http://localhost:8080/swagger-ui/index.html#/"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "MoneyBank Accounts microservice REST API documentation",
+				url = "http://localhost:8080/swagger-ui/index.html#/"
+		)
+)
 public class AccountsApplication {
 
 	public static void main(String[] args) {
