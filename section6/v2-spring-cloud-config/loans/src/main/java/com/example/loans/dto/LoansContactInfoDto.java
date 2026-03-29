8befe189a5c2b15349ab6a740aae34faf01ca2cb
sandeeprawat983 <rawat683sandeep@gmail.com>
2026-03-29 13:51:00 +0530

    refresh configuration with actuator refresh path

--- section6/v2-spring-cloud-config/loans/src/main/java/com/example/loans/dto/LoansContactInfoDto.java
+++ section6/v2-spring-cloud-config/loans/src/main/java/com/example/loans/dto/LoansContactInfoDto.java
@@ -6,5 +6,42 @@ import java.util.List;
 import java.util.Map;
 
 @ConfigurationProperties(value = "loans")
-public record LoansContactInfoDto(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+public class LoansContactInfoDto {
+
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Map<String, String> getContactDetails() {
+        return contactDetails;
+    }
+
+    public void setContactDetails(Map<String, String> contactDetails) {
+        this.contactDetails = contactDetails;
+    }
+
+    public List<String> getOnCallSupport() {
+        return onCallSupport;
+    }
+
+    public void setOnCallSupport(List<String> onCallSupport) {
+        this.onCallSupport = onCallSupport;
+    }
+
+    @Override
+    public String toString() {
+        return "LoansContactInfoDto{" +
+                "message='" + message + '\'' +
+                ", contactDetails=" + contactDetails +
+                ", onCallSupport=" + onCallSupport +
+                '}';
+    }
 }
