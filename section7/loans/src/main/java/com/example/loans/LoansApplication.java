41768af44c8f05d9cc8840a46dc40c784a8e77e5
sandeeprawat983 <rawat683sandeep@gmail.com>
2026-04-18 15:02:50 +0530

    replace H2 DB with MySQL DB and update docker compose file with MySQL DB

---
+++ section7/loans/src/main/java/com/example/loans/LoansApplication.java
@@ -0,0 +1,43 @@
+package com.example.loans;
+
+import com.example.loans.dto.LoansContactInfoDto;
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@EnableConfigurationProperties(value = {LoansContactInfoDto.class})
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Loans microservice REST API documentation",
+				description = "MoneyBank Loans microservice REST API documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Sandeep Rawat",
+						email = "microservice@abcd.com",
+						url = "http://localhost:8080/swagger-ui/index.html#/"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "http://localhost:8080/swagger-ui/index.html#/"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "MoneyBank Loans microservice REST API documentation",
+				url = "http://localhost:8080/swagger-ui/index.html#/"
+		)
+)
+public class LoansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoansApplication.class, args);
+	}
+
+}
