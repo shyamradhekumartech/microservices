b4ea323680cdc141ba1516e007c213bed873c8e9
sandeeprawat983 <rawat683sandeep@gmail.com>
2026-05-20 17:34:05 +0530

    implementing cross cutting tracing and logging using gateway

--- section9/accounts/src/main/java/com/moneybanks/accounts/service/impl/CustomerServiceImpl.java
+++ section9/accounts/src/main/java/com/moneybanks/accounts/service/impl/CustomerServiceImpl.java
@@ -38,7 +38,7 @@ public class CustomerServiceImpl implements ICustomersService {
      * @return Customer Details based on a given mobileNumber
      */
     @Override
-    public CustomerDetailsDto fetchCustomerDetails(String mobileNumber) {
+    public CustomerDetailsDto fetchCustomerDetails(String mobileNumber, String correlationId) {
         Customer customer = customerRepository.findByMobileNumber(mobileNumber)
                 .orElseThrow(
                         () -> new ResourceNotFoundException("Customer", "mobileNumber", mobileNumber)
@@ -51,10 +51,10 @@ public class CustomerServiceImpl implements ICustomersService {
         CustomerDetailsDto customerDetailsDto = CustomerMapper.mapToCustomerDetailsDto(customer, new CustomerDetailsDto());
         customerDetailsDto.setAccountsDto(AccountMapper.mapToAccountsDto(accounts, new AccountsDto()));
 
-        ResponseEntity<LoansDto> loansDtoResponseEntity = loansFeignClient.fetchLoanDetails(mobileNumber);
+        ResponseEntity<LoansDto> loansDtoResponseEntity = loansFeignClient.fetchLoanDetails(correlationId, mobileNumber);
         customerDetailsDto.setLoansDto(loansDtoResponseEntity.getBody());
 
-        ResponseEntity<CardsDto> cardsDtoResponseEntity = cardsFeignClient.fetchCardDetails(mobileNumber);
+        ResponseEntity<CardsDto> cardsDtoResponseEntity = cardsFeignClient.fetchCardDetails(correlationId, mobileNumber);
         customerDetailsDto.setCardsDto(cardsDtoResponseEntity.getBody());
 
         return customerDetailsDto;
