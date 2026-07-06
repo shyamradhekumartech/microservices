a25d6b6c7d2e71972f706d8220e8139258707738
sandeeprawat983 <rawat683sandeep@gmail.com>
2026-07-06 18:19:17 +0530

    implement redis ratelimter with gateway server

--- section10/gatewayserver/src/main/java/com/moneybank/gatewayserver/GatewayserverApplication.java
+++ section10/gatewayserver/src/main/java/com/moneybank/gatewayserver/GatewayserverApplication.java
@@ -2,10 +2,13 @@ package com.moneybank.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
+import reactor.core.publisher.Mono;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
@@ -28,7 +31,9 @@ public class GatewayserverApplication {
 						.uri("lb://ACCOUNTS"))
 				.route(p -> p.path("/moneybank/cards/**")
 						.filters(f -> f.rewritePath("/moneybank/cards/(?<segment>.*)", "/${segment}")
-								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+								.requestRateLimiter(config -> config.setRateLimiter(redisRateLimiter())
+										.setKeyResolver(userKeyResolver())))
 						.uri("lb://CARDS"))
 				.route(p -> p.path("/moneybank/loans/**")
 						.filters(f -> f.rewritePath("/moneybank/loans/(?<segment>.*)", "/${segment}")
@@ -38,6 +43,17 @@ public class GatewayserverApplication {
 										.setBackoff(Duration.ofMillis(100), Duration.ofMillis(1000), 2, true)))
 						.uri("lb://LOANS"))
 				.build();
+	}
+
+	@Bean
+	public RedisRateLimiter redisRateLimiter() {
+		return new RedisRateLimiter(1, 1, 1);
+	}
+
+	@Bean
+	KeyResolver userKeyResolver() {
+		return exchange -> Mono.justOrEmpty(exchange.getRequest().getHeaders().getFirst("user"))
+				.defaultIfEmpty("anonymous");
 	}
 
 }
